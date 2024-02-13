0a6f325381c3caf09850a37b85f943565bde62dc
Yunkon Kim <hermitkim1@gmail.com>
2024-02-13 20:05:06 +0900

    Update korean.go

--- test/korean.go
+++ test/korean.go
@@ -1,9 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+	"fmt"
+)
 
 func main() {
 	// 한글로 출력
-	println("안녕하세요, 세계!")
-	log.Println("안녕하세요, 세계!")
+	fmt.Println("안녕하세요, 세계?!")
+	log.Println("안녕하세요, 세계!!!")
+	// 한글 주석
+	fmt.Println("헬로")
 }
